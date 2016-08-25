548e1b66278e06fad3657859cf247fee7bdd0fc1
村上 勝俊 <murakami.katsutoshi@192.168.100.167>
2016-08-25 20:06:41 +0900

    :metal: ADD BASHRC

--- setup_dotfile.sh
+++ setup_dotfile.sh
@@ -3,4 +3,5 @@ basedir=$(cd $(dirname $0) && pwd)
 
 # Commit Template
 ln -fns ${basedir}/commit_template.txt ~/.commit_template
+ln -fns /Users/murakami.katsutoshi/workspace/dotfile/bashrc ~/.bashrc
 git config --global commit.template ~/.commit_template
