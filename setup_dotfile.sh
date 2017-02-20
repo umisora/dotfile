acb847cf36556e362b7bca865752584042dcf149
umisora <bit72s_akm@hotmail.com>
2017-02-20 18:28:46 +0900

    :up: Link Create Force

--- setup_dotfile.sh
+++ setup_dotfile.sh
@@ -10,11 +10,12 @@ ln -fns ${basedir}/vimrc ~/.vimrc
 ln -fns ${basedir}/gvimrc ~/.gvimrc
 ln -fns ${basedir}/tmux.conf ~/.tmux.conf
 ln -fns ${basedir}/themas/dircolors-solarized ~/.dircolors-solarized 
+ln -fns ${basedir}/gitconfig ~/.gitconfig
 mkdir -p  ~/.vim/colors/
 ln -fns ${basedir}/themas/vimcolor/solarized.vim ~/.vim/colors/
 
 cd /usr/local/etc/bash_completion.d  
-ln -s /Applications/Docker.app/Contents/Resources/etc/docker.bash-completion  
-ln -s /Applications/Docker.app/Contents/Resources/etc/docker-machine.bash-completion  
-ln -s /Applications/Docker.app/Contents/Resources/etc/docker-compose.bash-completion
+ln -fns /Applications/Docker.app/Contents/Resources/etc/docker.bash-completion  
+ln -fns /Applications/Docker.app/Contents/Resources/etc/docker-machine.bash-completion  
+ln -fns /Applications/Docker.app/Contents/Resources/etc/docker-compose.bash-completion
 cd -
